3932068f650dbb18dc8f875edd8806d4a310bb52
Mario-SO <gh_mario.proclaim@slmail.me>
2022-09-06 23:25:21 +0200

    First commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,38 +1,45 @@
 import '../styles/globals.css';
 import '@rainbow-me/rainbowkit/styles.css';
 import type { AppProps } from 'next/app';
-import { RainbowKitProvider, getDefaultWallets } from '@rainbow-me/rainbowkit';
-import { chain, configureChains, createClient, WagmiConfig } from 'wagmi';
+import {
+  RainbowKitProvider,
+  getDefaultWallets,
+  connectorsForWallets,
+  wallet,
+  darkTheme,
+  lightTheme,
+} from '@rainbow-me/rainbowkit';
+import { chain, createClient, configureChains, WagmiConfig } from 'wagmi';
 import { alchemyProvider } from 'wagmi/providers/alchemy';
 import { publicProvider } from 'wagmi/providers/public';
 
 const { chains, provider, webSocketProvider } = configureChains(
+  [chain.goerli],
   [
-    chain.mainnet,
-    chain.polygon,
-    chain.optimism,
-    chain.arbitrum,
-    ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === 'true'
-      ? [chain.goerli, chain.kovan, chain.rinkeby, chain.ropsten]
-      : []),
-  ],
-  [
-    alchemyProvider({
-      // This is Alchemy's default API key.
-      // You can get your own at https://dashboard.alchemyapi.io
-      apiKey: '_gg7wSSi0KMBsdKnGVfHDueq6xMB9EkC',
-    }),
+    alchemyProvider({ apiKey: 'bUQtqNq4FeXuyU-vT157iBiKrGPBy1S9' }),
     publicProvider(),
   ]
 );
 
-const { connectors } = getDefaultWallets({
-  appName: 'RainbowKit App',
+const { wallets } = getDefaultWallets({
+  appName: 'RainbowKit Mint NFT Demo',
   chains,
 });
 
+const demoAppInfo = {
+  appName: 'RainbowKit Mint NFT Demo',
+};
+
+const connectors = connectorsForWallets([
+  ...wallets,
+  {
+    groupName: 'Other',
+    wallets: [wallet.argent({ chains }), wallet.trust({ chains })],
+  },
+]);
+
 const wagmiClient = createClient({
-  autoConnect: true,
+  autoConnect: false,
   connectors,
   provider,
   webSocketProvider,
@@ -41,7 +48,10 @@ const wagmiClient = createClient({
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <WagmiConfig client={wagmiClient}>
-      <RainbowKitProvider chains={chains}>
+      <RainbowKitProvider theme={{
+      lightMode: lightTheme(),
+      darkMode: darkTheme(),
+    }} appInfo={demoAppInfo} chains={chains}>
         <Component {...pageProps} />
       </RainbowKitProvider>
     </WagmiConfig>
